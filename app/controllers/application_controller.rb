2448bef545350130c3fdbaa36410a11a77674e32
owenliversidge <owen.liversidge@gmail.com>
2018-04-09 14:41:50 -0400

    set up mixtape controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,39 @@
 class ApplicationController < ActionController::API
+
+    def index
+        @mixtapes = Mixtape.all
+        render json: @mixtapes
+    end
+
+    def create
+        @mixtape = Mixtape.create!(mixtape_params)
+
+        render json: @mixtape
+    end
+
+    def show 
+        @mixtape = Mixtape.find(params[:id])
+
+        render json: @mixtape
+    end
+
+    def update
+        @mixtape = Mixtape.find(params[:id])
+        @mixtape.update!(mixtape_params)
+
+        render json: @mixtape
+    end
+
+    def destroy
+        @mixtape = Mixtape.find(params[:id]).delete
+
+        render status: :ok
+    end
+
+    private
+
+    def mixtape_params
+        params.require(:mixtape).permit(:title, :created_by, :created_for, :dedication)
+    end
+
 end
