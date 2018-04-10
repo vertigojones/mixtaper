10972654ba7fb593abffc87edb80a16b8394adec
owenliversidge <owen.liversidge@gmail.com>
2018-04-10 10:20:26 -0400

    create new mixtape working

--- client/src/components/mixtapes/Mixtape.js
+++ client/src/components/mixtapes/Mixtape.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import { Card, Image, Grid, List, Divider, Button } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+import EditMixtapeForm from './EditMixtapeForm';
 
 class Mixtape extends Component {
     render() {
--- client/src/components/mixtapes/NewMixtapeForm.js
+++ client/src/components/mixtapes/NewMixtapeForm.js
@@ -1,13 +1,30 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { Form, Button } from 'semantic-ui-react'
 
 class NewMixtapeForm extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        );
-    }
+  render () {
+    return (
+      <Form onSubmit={this.props.createNewMixtape}>
+        <Form.Field>
+          <label>Title of Mixtape:</label>
+          <input placeholder='Title' name="title" onChange={this.props.handleChange} value={this.props.newMixtape.title}/>
+        </Form.Field>
+        <Form.Field>
+          <label>Created By:</label>
+          <input placeholder='Created By' name="created_by" onChange={this.props.handleChange} value={this.props.newMixtape.created_by}/>
+        </Form.Field>
+        <Form.Field>
+          <label>Created For:</label>
+          <input placeholder='Created For' name="created_for" onChange={this.props.handleChange} value={this.props.newMixtape.created_for}/>
+        </Form.Field>
+        <Form.Field>
+          <label>Dedication:</label>
+          <input placeholder='Dedication' name="dedication" onChange={this.props.handleChange} value={this.props.newMixtape.dedication}/>
+        </Form.Field>
+        <Button type='submit'>Submit</Button>
+      </Form>
+    )
+  }
 }
 
-export default NewMixtapeForm;
+export default NewMixtapeForm
