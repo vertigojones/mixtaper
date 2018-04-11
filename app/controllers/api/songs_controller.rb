25da85316e0388b846e0163129669b35f547d7c9
owenliversidge <owen.liversidge@gmail.com>
2018-04-10 22:42:00 -0400

    set up song controller

---
+++ app/controllers/api/songs_controller.rb
@@ -0,0 +1,47 @@
+class Api::SongsController < ApplicationController
+    def index
+        @mixtape = Mixtape.find(params[:mixtape_id])
+        @songs = @artist.songs
+        render json: {
+          songs: @songs
+        }
+      end
+    
+      def show
+        @song = Song.find(params[:id])
+        render json: {
+          song: @song
+        }
+      end
+    
+      def create
+        @mixtape = Mixtape.find(params[:mixtape_id])
+        @song = @mixtape.songs.create(song_params)
+        render json: {
+          song: @song
+        }
+      end
+    
+      def update
+        @mixtape = Mixtape.find(params[:mixtape_id])
+        @song = @mixtape.songs.find(params[:id])
+        @song.update!(song_params)
+        render json: {
+          song: @song
+        }
+      end
+    
+      def destroy
+        Song.find(params[:id]).destroy
+    
+        render json: {
+          message: "Song successfully destroyed"
+        }
+      end
+    
+      private
+    
+      def song_params
+        params.require(:song).permit(:artist, :title, :length )
+      end
+end
